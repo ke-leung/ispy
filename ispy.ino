e125fb0fd1fa182fc6f6902879df69c6cb6e03e5
Kate Leung <55610111+ke-leung@users.noreply.github.com>
2023-01-29 20:34:37 -0500

    Add files via upload

---
+++ ispy.ino
@@ -0,0 +1,25 @@
+/* Codebase for I SPY project
+
+Necessary components:
+Touch sensor implementation
+Reward System implementation
+ - LED feedback
+
+
+*/
+
+// Libraries
+
+// Pin Definition
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
